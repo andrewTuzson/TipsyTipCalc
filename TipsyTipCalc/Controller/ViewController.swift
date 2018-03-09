d83f2501289f5024f4abd3c993e75e67341c0ecb
Andrew Tuzson <andrew@evolempirecreative.com>
2018-03-09 05:29:42 -0600

    - finished MVC - tested functionality successfully

--- TipsyTipCalc/Controller/ViewController.swift
+++ TipsyTipCalc/Controller/ViewController.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    @IBOutlet weak var tipPercentSlider: UISlider!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    var tip = TipModel(billAmount: 0.0, tipPercent: 0.0)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setTipCalculationValues()
+        updateUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setTipCalculationValues() {
+        tip.tipPercent = Double(tipPercentSlider.value)
+        tip.billAmount = ((textField.text)! as NSString).doubleValue
+        tip.calculateTip()
     }
-
-
+    
+    func updateUI() {
+        tipLabel.text = String(format: "$%0.2f", tip.tipAmount)
+        totalLabel.text = String(format: "$%0.2f", tip.totalAmount)
+        tipPercentLabel.text = "Tip: \(Int(tipPercentSlider.value * 100))%"
+    }
+    
+    // Actions
+    @IBAction func billAmountDidChange(_ sender: Any) {
+        setTipCalculationValues()
+        updateUI()
+    }
+    
+    @IBAction func tipPercentageChanged(_ sender: Any) {
+        setTipCalculationValues()
+        updateUI()
+    }
+    
 }
 
